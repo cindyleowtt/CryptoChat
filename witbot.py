449fb0a6eb259fe29b879a7a63330db81ff5ed4e
cindyleowtt <cindyleowtt@minerva.kgi.edu>
2018-04-20 22:49:33 +0530

    Added dependencies and entity responses

--- witbot.py
+++ witbot.py
@@ -5,11 +5,9 @@ from pymessenger import Bot
 import os, sys
 
 """
-
 # ---- CRYPTO BOT ---- # 
 Messenger Bot built using Wit.ai 
 Returning Cryptocurrency Data & News
-
 """
 
 # Initialise Flask app 
@@ -52,70 +50,109 @@ def webhook():
 	Interface to the Messenger bot. 
 	"""
 	data = request.get_json()
+	print(data)
 	log(data)
 	if data['object'] == 'page':
-		for entry in data['entry']:
-			for event in entry['messaging']:
-				sender_id = event['sender']['id']
-				recipient_id = event['recipient']['id']
-				if event.get('message'):
-					if 'text' in event['message']:
-						text = event['message']['text']
-					else:
-						text = 'No Text'
-					response = None
-					entity, value = wit_response(text)
-					if entity == 'cryptoasset': 
-						response = 'hey, the price of {0} today is USD {1}'.format(value, get_prices(value))
-					if response == None: 
-						response = "Sorry, can you try again?"
-					bot.send_text_message(recipient_id, response)
+		messages = data['entry'][0]['messaging']
+		if messages:
+			message = messages[0]
+			sender_id = message['sender']['id']
+			recipient_id = message['recipient']['id']
+			if 'text' in message['message']:
+				msg = message['message']['text']
+			elif 'text' not in message['message']:
+				msg = 'no message'
+			response = client.message(msg=msg, context={'session_id': sender_id})
+			send = helper_response(response)
+			bot.send_text_message(sender_id, send)
+	else: 
+		"Received another event"
 	return "OK", 200
 
 def log(message):
+	"""
+	Prints out the message.
+	"""
 	print(message)
 	sys.stdout.flush()
 
-def get_entities(message):
-	"""
+
+def entity_val(entities, entity):
+	""" 
+	Searches for and returns the entities within the message. 
 	Takes the wit.ai response and tries to find the entities associated with it
 	+ the intent.
-	"""
-	resp = client.message(message)
 
-def helper_response(message, entities, value):
+	Example Client Response from Wit.Ai:
+	{'_text': "what's the price of btc", 
+	'entities': {'cryptoasset': 
+					[{'confidence': 1, 
+					'value': 'Bitcoin', 
+					'type': 'value'}], 
+				'intent': 
+					[{'confidence': 0.99314743230657, 
+					'value': 'get_price'}]}, 
+					'msg_id': '0vRhDxN0fiaNSM8Gb'}
+	"""
+	if entity not in entities:
+		return None
+	val = entities[entity][0]['value']
+	if not val:
+		return None
+	return val 
+
+def intent_val(entities, intent):
+	""" 
+	Returns the intent of the message.
+	"""
+	if 'intent' not in entities:
+		return None
+	else:
+		val = entities['intent'][0]['value']
+		if val == 'get_price':
+			return val
+		elif val == 'get_marketcondition':
+			return val
+	return val 
+
+def helper_response(response):
+	"""
+	This helper response defines the client response.
+	"""
 	entities = response['entities']
-	greetings = wit_response(message)[0]
+	get_price = intent_val(entities, 'get_price')
+	get_marketcondition = intent_val(entities, 'get_marketcondition')
+	cryptoasset = entity_val(entities, 'cryptoasset')
+	greetings = entity_val(entities, 'greetings')
+	if get_price and cryptoasset:
+		# We can make some assumptions -- if the 'intent' is to get price and 
+		# the user cites a cryptocurrency, we can assume that the user is asking
+		# for its price. 
+			return getprices(cryptoasset)
+	if get_marketcondition:
+		return getmarketcondition()
+	if greetings: 
+		return ("Hey, how can I help you?")
+	else: 
+		return ('I am not sure how to help you, try again!')
 
-
-def wit_response(message):
-	resp = client.message(message)
-	entity = list(resp['entities'])[0]
-	value = resp['entities'][entity][0]['value']
-	return entity, value
-
-def get_prices(currency):
+def getprices(currency):
 	"""
-	Returns the current price for a particular cryptoasset.
-	Retrieves from coinmarketcap.com's public API. 
+	Returns the price for a particular cryptoasset.
 	"""
-	try:
-		request = requests.get('https://api.coinmarketcap.com/v1/ticker/{}/'.format(currency))
-		prices = request.json()
-		price = prices[0]['price_usd']
-	except KeyError: 
-		return 'Sorry, what currency is that?'
-	return price
+	request = requests.get('https://api.coinmarketcap.com/v1/ticker/{}/'.format(currency))
+	prices = request.json()
+	return 'Here is the current price for {0}: {1}'.format(currency, prices[0]['price_usd'])
 
-def get_time_change(currency, time):
+def getmarketcondition():
 	"""
-	Returns the change in price. 
+	Returns the current market condition.
 	"""
-	pass
-
-def get_marketstate():
-	pass
+	request = requests.get('https://api.coinmarketcap.com/v1/global/')
+	condition = request.json()
+	marketcap = condition['total_market_cap_usd']
+	totalvolume = condition['total_24h_volume_usd']
+	return 'The market cap is now {0}, and the volume for past 24 hours is {1}'.format(marketcap, totalvolume)
 
 if __name__ == '__main__':
 	app.run(debug=True)
-
